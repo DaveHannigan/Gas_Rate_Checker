e180aee6860c02905915703fa09afbd79c88f471
Dave Hannigan <dave.hannigan1@live.co.uk>
2019-12-16 08:58:46 +0000

    2nd attemp

--- app/src/main/java/com/example/gasratechecker/MainActivity.kt
+++ app/src/main/java/com/example/gasratechecker/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.gasratechecker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.TextView
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.sql.Time
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    var startTime = 0L
+    var stopTime = 0L
+
+    fun start(view: View) {
+        startTime = timer()
+        val time = findViewById<TextView>(R.id.hello)
+        time.text = "0"
+    }
+    fun stop(view: View){
+        stopTime = timer()
+        var elapsedTime: Double  = (stopTime-startTime)/1000.toDouble()
+        val time = findViewById<TextView>(R.id.hello)
+        val normMeter = findViewById<TextView>(R.id.normal_meter_gas_rate)
+        time.text = elapsedTime.toString()
+        var gasUsageM2 = BigDecimal(0.01/elapsedTime*3600).setScale(2,RoundingMode.HALF_EVEN)
+        var gasUsageKw =  BigDecimal(10.77*gasUsageM2).setScale(2, RoundingMode.HALF_EVEN)
+        normMeter.text = gasUsageM2.toString()
+    }
+
 }
+
+fun timer() :Long{
+    return SystemClock.elapsedRealtime()
+}
+
+
+
